f9883b6e4b56d130584b7924acfabd311edd8872
hpwit <bazin.yves@gmail.com>
2019-02-20 23:00:01 +0100

    Update test.ino

--- test.ino
+++ test.ino
@@ -3,6 +3,7 @@
 
 
 #include "FastLED.h"
+#inlcude "animation.h"
 FASTLED_USING_NAMESPACE
 
 #define NUM_LEDS 68
@@ -14,18 +15,27 @@ CRGB leds[NUM_LEDS];
 
 class myAnimation2 : public Animation
  {
-   private:
+ 
+ /* 
+ once in that class it's like being in a separate program
+ where the led strip is 'ledsa' and the length of this strip is 'num_leds'
+ you can see this strip as independant
+ */
+   private://do not touch
+   //here define the 'global' variable that could change at every loop
     int k;
-   public:
-    using Animation::Animation;
-    //myAnimation ( CRGB *ext_leds,int start_led,int end_led):Animation(  ext_leds ,start_led,end_led,&this->animation);
+  
+  public://do not touch
+    using Animation::Animation; //do not touch
+    
     void setup()
-    {
+    { //put here the code if you need to initialized the variables this it not necessary
+      fill_solid(ledsa,num_leds,CRGB::Blue);
       k=300;
     }
-    void animation() override
+    void animation() override //this is the function that will be executed each time
     {
-      //Serial.println("ee");
+      //change the code below
       for(int i=0;i<=num_leds;i++)
       {
         ledsa[i]=CHSV((i+k)*255/num_leds,255,255);
@@ -34,7 +44,9 @@ class myAnimation2 : public Animation
     } 
     
  };
- 
+
+
+//a second animation 
 class myAnimation : public Animation
  {
    private:
@@ -49,7 +61,6 @@ class myAnimation : public Animation
     void animation() override
     {
       fill_solid(ledsa,num_leds,CRGB::Green);
-      //Serial.println("ee");
       for(int i=0;i<=k;i++)
       {
         ledsa[i]=CRGB::Red;
@@ -62,18 +73,18 @@ class myAnimation : public Animation
 
 
 
- myAnimation anim=myAnimation(leds,0,20);
+ myAnimation anim=myAnimation(leds,0,20); // we initialize the animation (the global led strip, the first led, the last led)
 myAnimation2 anim2=myAnimation2(leds,21,49);
 
 void setup() {
 
   FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS);
-    anim.setup():
-   anim.setup():
+    anim.setup();
+   anim2.setup();
 }
 
 void loop() {
-  anim.lopp();
+  anim.loop(); //to execute the animation
   anim.loop();
 
 }
